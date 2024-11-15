b409ecbd6b16546fab87e298376916bc35e1b239
Hershey Marbeda <marbedahershey@gmail.com>
2024-11-15 18:15:13 -0500

    Initial Commit

---
+++ CSCI202-M04Search-Sorts/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CSCI202-M04Search-Sorts
+//
+//  Created by Hershey Marbeda on 11/15/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
